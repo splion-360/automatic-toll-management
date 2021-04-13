028cdb6a87d0b9c330a2dcfde6c96e6c0a071bae
Vignesh-Desmond <vignesh.desmond@gmail.com>
2021-04-13 11:37:56 +0530

    get instance IP

--- apireq.py
+++ apireq.py
@@ -1,9 +1,27 @@
+import os
 import urllib.request
 import requests
 import numpy as np
 import time
-fastapi_post_url = "http://65.2.33.175:8080/predict"
+import boto3
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# CONNECTION TO API
+ACCESS_ID = os.getenv("ACCESS_ID")
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+ec2 = boto3.client('ec2',region_name='ap-south-1', aws_access_key_id=ACCESS_ID,
+         aws_secret_access_key= ACCESS_KEY)
+response = ec2.describe_instances()
+if response["Reservations"][0].get(u"Instances")[0].get(u"PublicIpAddress") is not None:
+    ip_address = response["Reservations"][0].get(u"Instances")[0].get(u"PublicIpAddress")
+else:
+    print("Unable to get instance IP address")
+
+fastapi_post_url = f"http://{ip_address}:8080/predict"
+
+# POSTING LOCAL IMAGE
 imgResp=urllib.request.urlopen('file:' + r'‪C:\Users\vigne\Downloads\ezgif-frame-001.jpg')
 imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8) 
 start = time.time()
